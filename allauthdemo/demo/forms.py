e391398e1a714aab54d3baf9baeb675f8b0282b0
Md. Rana Mahmud <ranamd0@gmail.com>
2017-03-15 20:44:24 +0600

    Shahparan Hall text added

--- allauthdemo/demo/forms.py
+++ allauthdemo/demo/forms.py
@@ -6,7 +6,7 @@ class LibraryFineForm(forms.ModelForm):
         model = LibraryFine
         # fields = '__all__'
         fields = ['date','gender','book_count','amount_fined']
-
+        widgets = {'date': forms.DateInput()}
 
 
 class ShahparanHallForm(forms.ModelForm):
@@ -14,34 +14,40 @@ class ShahparanHallForm(forms.ModelForm):
     class Meta:
         model = ShahparanHall
         fields = '__all__'
-        exclude = ['user']
+        exclude = ['user','created_date','published_date']
+        widgets = {'date': forms.DateInput()}
 class TranscriptForm(forms.ModelForm):
     
     class Meta:
         model = Transcript
         fields = '__all__'
-        exclude = ['user']
+        exclude = ['user','created_date','published_date']
+        widgets = {'date': forms.DateInput()}
 class GradesheeteForm(forms.ModelForm):
     
     class Meta:
         model = Gradesheet
         fields = '__all__'
-        exclude = ['user']
+        exclude = ['user','created_date','published_date']
+        widgets = {'date': forms.DateInput()}
 class CashMemoForm(forms.ModelForm):
     
     class Meta:
         model = CashMemo
         fields = '__all__'
-        exclude = ['user']
+        exclude = ['user','created_date','published_date']
+        widgets = {'date': forms.DateInput()}  
 class S2Form(forms.ModelForm):
     
     class Meta:
         model = S2
         fields = '__all__'
-        exclude = ['user']
+        exclude = ['user','created_date','published_date']
+        widgets = {'date': forms.DateInput()}
 class STD6Form(forms.ModelForm):
     
     class Meta:
         model = STD6
         fields = '__all__'
-        exclude = ['user']
+        exclude = ['user','created_date','published_date']
+        widgets = {'date': forms.DateInput()}
